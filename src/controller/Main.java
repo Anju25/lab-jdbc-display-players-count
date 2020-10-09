a1e17efde7b81c3324a9e9b251795800e72a2c28
Anju <anju02051998@gmail.com>
2020-10-09 11:17:05 +0530

    prograd

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,28 @@
+package controller;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+//import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import dao.SkillDAO;
+//import model.Conference;
+import model.Skill;
+
+public class Main
+{
+	public static HashMap<Skill,Integer> hashmap=new HashMap<Skill,Integer>();
+	public static void main(String args[]) throws ClassNotFoundException, SQLException
+	{
+		SkillDAO skilldao=new SkillDAO();
+		hashmap=skilldao.skillCount();
+		//TreeMap<Skill,Integer> map=new TreeMap<Skill,Integer>(hashmap);
+	    
+		for(Entry<Skill, Integer> pair: hashmap.entrySet())
+		{
+		   Skill s=pair.getKey();
+           int value=pair.getValue();	
+	      System.out.println(s.getSkillname()+" "+value);
+		}
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,42 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	public static HashMap<Skill,Integer> hashmap=new HashMap<Skill,Integer>();
+	
+	public HashMap<Skill,Integer> skillCount() throws ClassNotFoundException, SQLException
+	{
+		Connection con=ConnectionManager.getConnection();
+		if(con!=null)
+		{
+			String sql="select skillname,count(*) as count from skilldetails group by skillname";
+			Statement st=con.createStatement();
+			ResultSet rs=st.executeQuery(sql);
+			while(rs.next())
+			{
+			   //int skillid=rs.getInt(1);
+			   String name=rs.getString("skillname");
+			   int count=rs.getInt("count");
+			   Skill s=new Skill(count,name);
+			   //String s1=s.getSkillname();
+			   hashmap.put(s,count);
+			}
+					
+		}
+		return hashmap;
+		
+	}
+	
+	
+	
+	
+}
